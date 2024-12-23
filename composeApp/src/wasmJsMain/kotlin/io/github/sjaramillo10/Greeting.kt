019180301aa8b3d936011cdd1b34b04c8835184b
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2024-12-22 21:39:41 -0600

    Add kotlin wasm project

---
+++ composeApp/src/wasmJsMain/kotlin/io/github/sjaramillo10/Greeting.kt
@@ -0,0 +1,9 @@
+package io.github.sjaramillo10
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/wasmJsMain/kotlin/io/github/sjaramillo10/Platform.kt
@@ -0,0 +1,7 @@
+package io.github.sjaramillo10
+
+class WasmPlatform {
+    val name: String = "Web with Kotlin/Wasm"
+}
+
+fun getPlatform() = WasmPlatform()
